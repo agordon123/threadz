145d4866b588ba48e3e074e7b218efcb78f36aac
Adam Gordon <adamg42685@gmail.com>
2023-08-22 14:09:58 -0400

    assets

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -10,7 +10,7 @@ const LeftSidebar=()=>{
     const pathname = usePathname();
 
     return (
-        <section className="custom-scrollbar-leftsidebar">
+        <section className="custom-scrollbar leftsidebar">
         <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
           const isActive =
@@ -37,11 +37,12 @@ const LeftSidebar=()=>{
           );
         })}
         </div>
-        <div className="">
+        <div className="mt-10 px-6">
         <SignedIn>
-                <SignOutButton>
-                    <div className="flex cursor-pointer">
-                        <Image src="/logout.svg" alt="logout" width={28} height={28} />
+                <SignOutButton signOutCallback={()=>router.push('/sign-in')}>
+                    <div className="flex cursor-pointer gap-4 p-4">
+                        <Image src="/assets/logout.svg" alt="logout" width={28} height={28} />
+                        <p className="text-light-2 max-lg:hidden">Logout</p>
                     </div>
                 </SignOutButton>
             </SignedIn>
